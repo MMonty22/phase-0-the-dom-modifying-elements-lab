ac66cb2008a736eeae228b58d87c9c52459e98f4
Michael <michael.c.montgomery@gmail.com>
2022-02-26 12:28:16 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+  
+//with \"YOUR-NAME is the champion\" inside"
+//with a sweet message in it").to.include("is the champion")
+
+const main = document.getElementById('main')
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "YOUR-NAME is the champion"
